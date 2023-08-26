d2d59635e8223f5b4fbb9b797d564b1bccb74b6a
iruanwn <142493558+iruanwn@users.noreply.github.com>
2023-08-26 06:36:51 -0300

    Mandando pro repositorio

--- src/praticando/git/pkg2/PraticandoGit2.java
+++ src/praticando/git/pkg2/PraticandoGit2.java
@@ -4,7 +4,7 @@ package praticando.git.pkg2;
 public class PraticandoGit2 {
 
     public static void main(String[] args) {
-        System.out.println("Segundo commit");
+        System.out.println("Versionando para o repositorio");
     }
     
 }
